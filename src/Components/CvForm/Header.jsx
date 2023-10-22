2c2a9586858eab4358cbf2dd85876a6766dec935
sebnemgormus <sebnemgormus@gmail.com>
2023-10-22 18:46:56 +0300

    header updated

--- src/Components/CvForm/Header.jsx
+++ src/Components/CvForm/Header.jsx
@@ -1,21 +1,10 @@
 import github from "../../assets/github.svg"; 
-import autofill from "../../assets/autofill.svg"; 
-import save from "../../assets/save.svg"; 
-
 
 function Header() {
   return (
     <header>
       <div className="header">
         <h1>Cv Generator</h1>
-        <div className="headerBtnSection">
-          <button>
-            <img src={save} alt="" /><span>Download</span>
-          </button>
-          <button>
-            <img src={autofill} alt="" /><span>Autofill</span>
-          </button>
-        </div>
       </div>
       <div className="createdBy">
         Created by <span>
